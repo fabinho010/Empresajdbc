ade542104052f391327c4eddd2597a6ad736f6a9
fabinho190 <ossai10fabianfbn@gmail.com>
2023-04-21 20:19:44 +0200

    Arreglo 2

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,9 +2,15 @@ package com.example;
 
 import com.zaxxer.hikari.HikariDataSource;
 
+import java.sql.Connection;
+
 public class Application {
+    //Para plain jdbc
+    private static Connection connection;
     //Para el pool
     private static HikariDataSource dataSource;
+    //Para el JPA
+    private static JpaService jpaService = JpaService.getInstance();
     public static void main(String[] args) {
 
     }
