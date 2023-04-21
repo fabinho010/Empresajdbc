16a770dd390a3a8a7547101b3a6409c9ad0a3807
fabinho190 <ossai10fabianfbn@gmail.com>
2023-04-21 19:58:19 +0200

    Arreglo

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,5 +1,11 @@
 package com.example;
 
-public class Application {
+import com.zaxxer.hikari.HikariDataSource;
 
+public class Application {
+    //Para el pool
+    private static HikariDataSource dataSource;
+    public static void main(String[] args) {
+
+    }
 }
